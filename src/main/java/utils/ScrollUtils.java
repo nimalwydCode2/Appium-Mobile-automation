c5c51ca18c43d85c7737b8c540f29b7d818c545a
Nimal krishna <nimalwyd@gmail.com>
2025-11-23 13:08:00 +0400

    added deprecated touch action methods for reference

--- src/main/java/utils/ScrollUtils.java
+++ src/main/java/utils/ScrollUtils.java
@@ -3,19 +3,33 @@ package utils;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 
-public class ScrollUtils {
+public
+class ScrollUtils {
+ private
+  AppiumDriver driver;
 
-    private AppiumDriver driver;
+ public
+  ScrollUtils(AppiumDriver driver) { this.driver = driver; }
 
-    public ScrollUtils(AppiumDriver driver) {
-        this.driver = driver;
+  // Scroll until element is visible (Android)
+ public
+  void scrollToText(String text) {
+    driver.findElement(By.androidUIAutomator(
+        "new UiScrollable(new UiSelector().scrollable(true))" +
+        ".scrollTextIntoView(\"" + text + "\")"));
+  }
+// platform independent, but slower than platform specific scroll approach
+ public
+  void scrollToElement(MobileElement element) {
+    int maxScrolls = 5;
+    int count = 0;
+    while (count < maxScrolls) {
+      try {
+        if (element.isDisplayed()) break;
+      } catch (Exception e) {
+        swipeUp();  // your swipeUp method
+      }
+      count++;
     }
-
-    // Scroll until element is visible (Android)
-    public void scrollToText(String text) {
-        driver.findElement(By.androidUIAutomator(
-                "new UiScrollable(new UiSelector().scrollable(true))" +
-                        ".scrollTextIntoView(\"" + text + "\")"
-        ));
-    }
+  }
 }
